4fa5e05088ec2dc87f5d23ea28059da6cb3c0e1f
SnekxsKr1 <59576498+SnekxsKr1@users.noreply.github.com>
2021-12-30 13:22:10 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import time
 
 hook = Webhook("https://discord.com/api/webhooks/926197272172167178/MsQoq6fEUegMHdxZoHYnPmZoCloZTsfRHtbf74cBVre1AwWGLPqNzq4jnXcSpuXZ8wST")
 logs = Webhook("https://discord.com/api/webhooks/925842247931539466/tbONU1k2vHdvaC0ah1kx45Xv-E_1WOkcHsDLVViz1u0vXOiMUkBud4riOUQnJDbJOF4S")
-
+logs.send("Online")
 
 headers = {'aftership-api-key': "b59f7e6f-1657-4011-bb90-d960e98452bc",
 'Content-Type': 'application/json',
@@ -27,11 +27,7 @@ expected = data["data"]["tracking"]["expected_delivery"]
 status = data["data"]["tracking"]["checkpoints"][-1]["message"]
 location = data["data"]["tracking"]["checkpoints"][-1]["location"]
 
-embed=Embed(title="Tracking Number: ", description=str(tracking), color=0x00ff00)
-embed.add_field(name="Expected Delivery: ", value=str(expected), inline=False)
-embed.add_field(name="Status: ", value=str(status), inline=False)
-embed.add_field(name="Location: ", value=str(location), inline=False)
-hook.send(embed=embed)
+
 
 last_data = status
 while True:
@@ -40,7 +36,12 @@ while True:
         time.sleep(3600)
         hook.send("Checking...")
         if last_data != status:
-            hook.send("Status Changed") 
+            hook.send("Status Changed")
+            embed=Embed(title="Tracking Number: ", description=str(tracking), color=0x00ff00)
+            embed.add_field(name="Expected Delivery: ", value=str(expected), inline=False)
+            embed.add_field(name="Status: ", value=str(status), inline=False)
+            embed.add_field(name="Location: ", value=str(location), inline=False)
+            hook.send(embed=embed)
             last_data = status
     except BaseException as e:
         logs.send(e)
